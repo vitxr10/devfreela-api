838f25cee88edcf5d05840e3891a56496ac3ae72
vitor <vitorsalves0812@gmail.com>
2024-01-13 18:24:21 -0300

    criado as controllers project e users

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //return NotFound();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Project project)
+        {
+            //return BadRequest();
+            return CreatedAtAction(nameof(GetById), new {id = project.Id}, project);
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] Project project)
+        {
+            //return BadRequest();
+            // return NotFound();
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Project project)
+        {
+            ////return BadRequest();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //return BadRequest();
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.API/Models/Project.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace DevFreela.API.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
