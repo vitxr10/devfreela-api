62a3745e0f16661dbfbd7b470118d6eba30484f7
vitor <vitorsalves0812@gmail.com>
2024-01-17 21:33:32 -0300

    Configurado os relacionamentos de entidades

--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -25,6 +25,7 @@ namespace DevFreela.Core.Entities
         public DateTime BirthDate { get; private set; }
         public bool Active { get; set; }
         public List<UserSkill> Skills { get; private set; }
+        public List<ProjectComment> Comments { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
         public List<Project> FreelanceProjects { get; set; }
     }
--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -17,5 +17,70 @@ namespace DevFreela.Infrastructure.Persistence
         public DbSet<Skill> Skills { get; set; }
         public DbSet<UserSkill> UserSkills { get; set; }
         public DbSet<ProjectComment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .ToTable("db.Projects");
+
+            modelBuilder.Entity<Project>()
+                .HasKey(p => p.Id);
+
+            //relacionamento project freelancer
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.Freelancer)
+                .WithMany(f => f.FreelanceProjects)
+                .HasForeignKey(p => p.IdFreelancer)
+                .OnDelete(DeleteBehavior.Restrict); //não permite deletar dados que tem tabelas que dependem dele
+
+            //relacionamento project freelancer
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.Client)
+                .WithMany(c => c.OwnedProjects)
+                .HasForeignKey(p => p.IdClient)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProjectComment>()
+                .ToTable("db.ProjectComments");
+
+            modelBuilder.Entity<ProjectComment>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<ProjectComment>()
+                .HasOne(p => p.Project)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(p => p.IdProject)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProjectComment>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(p => p.IdUser)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .ToTable("db.Users");
+
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Skills)
+                .WithOne()
+                .HasForeignKey(u => u.IdSkill)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<UserSkill>()
+                .ToTable("db.UserSkills");
+
+            modelBuilder.Entity<UserSkill>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<UserSkill>()
+                .HasOne(s => s.User)
+                .WithMany(u => u.Skills)
+                .HasForeignKey(s => s.IdUser)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
