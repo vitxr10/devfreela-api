c5a39bdfe36ff0961c73f7adcf75199de8a6b7c1
vitor <vitorsalves0812@gmail.com>
2024-02-12 20:48:00 -0300

    Implementando o padrão Repository

---
+++ DevFreela.Application/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CreateUser
+{
+    public class CreateUserCommand : IRequest<int>
+    {
+        public CreateUserCommand(string fullName, string email, DateTime birthDate)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+    }
+}
