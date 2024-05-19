c0171545015a46933444af268da2f1ddfe61075f
vitor <vitorsalves0812@gmail.com>
2024-05-19 18:35:02 -0300

    feat: Adicionado informações de pagamentos no FinishProjectCommand

--- DevFreela.Application/Commands/FinishProject/FinishProjectCommand.cs
+++ DevFreela.Application/Commands/FinishProject/FinishProjectCommand.cs
@@ -20,5 +20,9 @@ namespace DevFreela.Application.Commands.FinishProject
         }
 
         public int Id { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string Cvv { get; set; }
+        public string ExpiresAt { get; set; }
+        public string FullName { get; set; }
     }
 }
